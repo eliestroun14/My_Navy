c17ad23388e57b7ebb15e3435e38fdd079c1bf92
nitrached <manech.dubreil@epitech.eu>
2024-01-15 18:29:25 +0100

    mod(./) str_to_word_array

--- lib/lib/my/my_str_to_word_array.c
+++ lib/lib/my/my_str_to_word_array.c
@@ -4,12 +4,11 @@
 ** File description:
 ** my_str_to_word_array.c
 */
-
 #include "my.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 
-char char_word(char const *str, int n, int nl, char ch)
+char char_word(char *str, int n, int nl, char *delimiters)
 {
     int i = 0;
     int sp = 1;
@@ -17,20 +16,20 @@ char char_word(char const *str, int n, int nl, char ch)
     int let = 0;
 
     while (str[i] != '\0') {
-        if (sp == 1 && str[i] != ch) {
+        if (sp == 1 && !my_strchr(delimiters, str[i])) {
             sp = 0;
             nb++;
         }
-        sp = (str[i] == ch) ? 1 : 0;
-        let += (str[i] != ch && nb == n) ? 1 : 0;
+        sp = (my_strchr(delimiters, str[i])) ? 1 : 0;
+        let += (!my_strchr(delimiters, str[i]) && nb == n) ? 1 : 0;
         if (let == nl)
-            return (str[i]);
+            return str[i];
         i++;
     }
-    return ('\0');
+    return '\0';
 }
 
-int nb_char(char const *str, int n, char ch)
+int nb_char(char *str, int n, char *delimiters)
 {
     int i = 0;
     int sp = 1;
@@ -38,50 +37,50 @@ int nb_char(char const *str, int n, char ch)
     int let = 0;
 
     while (str[i] != '\0') {
-        if (sp == 1 && str[i] != ch) {
+        if (sp == 1 && !my_strchr(delimiters, str[i])) {
             sp = 0;
             nb++;
         }
-        sp = (str[i] == ch) ? 1 : 0;
-        let += (str[i] != ch && nb == n) ? 1 : 0;
+        sp = (my_strchr(delimiters, str[i])) ? 1 : 0;
+        let += (!my_strchr(delimiters, str[i]) && nb == n) ? 1 : 0;
         i++;
     }
-    return (let);
+    return let;
 }
 
-int nb_word(char const *str, char ch)
+int nb_word(char *str, char *delimiters)
 {
     int i = 0;
     int sp = 1;
     int nb = 0;
 
     while (str[i] != '\0') {
-        if (sp == 1 && str[i] != ch) {
+        if (sp == 1 && !my_strchr(delimiters, str[i])) {
             sp = 0;
             nb++;
         }
-        sp = (str[i] == ch) ? 1 : 0;
+        sp = (my_strchr(delimiters, str[i])) ? 1 : 0;
         i++;
     }
-    return (nb);
+    return nb;
 }
 
-char **my_str_to_word_array(char const *str, char ch)
+char **my_str_to_word_array(char *str, char *delimiters)
 {
     int y = 0;
     int x = 0;
-    char **tab = malloc(sizeof(char *) * (nb_word(str, ch) + 1));
+    char **tab = malloc(sizeof(char *) * (nb_word(str, delimiters) + 1));
 
-    while (y < nb_word(str, ch)) {
-        tab[y] = malloc(sizeof(char) * (nb_char(str, y + 1, ch) + 1));
+    while (y < nb_word(str, delimiters)) {
+        tab[y] = malloc(sizeof(char) * (nb_char(str, y + 1, delimiters) + 1));
         x = 0;
-        while (x < nb_char(str, y + 1, ch)) {
-            tab[y][x] = char_word(str, y + 1, x + 1, ch);
+        while (x < nb_char(str, y + 1, delimiters)) {
+            tab[y][x] = char_word(str, y + 1, x + 1, delimiters);
             x++;
         }
         tab[y][x] = '\0';
         y++;
     }
     tab[y] = 0;
-    return (tab);
+    return tab;
 }
